13dee08c5822c1f85bed1020ba0acfe555a88824
brynmailer-mdc <bdm@mulgadc.com>
2026-07-09 16:51:24 +1000

    feat: finished sigv4 parser

---
+++ pkg/sigv4/sigv4.go
@@ -0,0 +1,208 @@
+package sigv4
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Scope struct {
+	Region  string
+	Service string
+}
+
+const (
+	amzTimeFormat = "20060102T150405Z"
+	amzDateFormat = "20060102"
+
+	MaxClockSkew  = 15 * time.Minute
+	MaxPresignAge = 7 * 24 * time.Hour
+)
+
+type Algorithm string
+
+const (
+	AlgorithmV4  Algorithm = "AWS4-HMAC-SHA256"
+	AlgorithmV4a Algorithm = "AWS4-ECDSA-P256-SHA256"
+)
+
+type Region string
+
+const (
+	RegionUSEast2      Region = "us-east-2"
+	RegionUSEast1      Region = "us-east-1"
+	RegionUSWest1      Region = "us-west-1"
+	RegionUSWest2      Region = "us-west-2"
+	RegionAFSouth1     Region = "af-south-1"
+	RegionAPEast1      Region = "ap-east-1"
+	RegionAPSouth2     Region = "ap-south-2"
+	RegionAPSoutheast3 Region = "ap-southeast-3"
+	RegionAPSoutheast5 Region = "ap-southeast-5"
+	RegionAPSoutheast4 Region = "ap-southeast-4"
+	RegionAPSouth1     Region = "ap-south-1"
+	RegionAPSoutheast6 Region = "ap-southeast-6"
+	RegionAPNortheast3 Region = "ap-northeast-3"
+	RegionAPNortheast2 Region = "ap-northeast-2"
+	RegionAPSoutheast1 Region = "ap-southeast-1"
+	RegionAPSoutheast2 Region = "ap-southeast-2"
+	RegionAPEast2      Region = "ap-east-2"
+	RegionAPSoutheast7 Region = "ap-southeast-7"
+	RegionAPNortheast1 Region = "ap-northeast-1"
+	RegionCACentral1   Region = "ca-central-1"
+	RegionCAWest1      Region = "ca-west-1"
+	RegionEUCentral1   Region = "eu-central-1"
+	RegionEUWest1      Region = "eu-west-1"
+	RegionEUWest2      Region = "eu-west-2"
+	RegionEUSouth1     Region = "eu-south-1"
+	RegionEUWest3      Region = "eu-west-3"
+	RegionEUSouth2     Region = "eu-south-2"
+	RegionEUNorth1     Region = "eu-north-1"
+	RegionEUCentral2   Region = "eu-central-2"
+	RegionILCentral1   Region = "il-central-1"
+	RegionMXCentral1   Region = "mx-central-1"
+	RegionMESouth1     Region = "me-south-1"
+	RegionMECentral1   Region = "me-central-1"
+	RegionSAEast1      Region = "sa-east-1"
+	RegionUSGovEast1   Region = "us-gov-east-1"
+	RegionUSGovWest1   Region = "us-gov-west-1"
+)
+
+type Service string
+
+const (
+	ServiceS3  Service = "s3"
+	ServiceACM Service = "acm"
+	ServiceEC2 Service = "ec2"
+	ServiceECR Service = "ecr"
+	ServiceECS Service = "ecs"
+	ServiceEKS Service = "eks"
+	ServiceIAM Service = "iam"
+)
+
+type Payload int
+
+const (
+	PayloadSigned Payload = iota
+	PayloadUnsigned
+	PayloadStreamingSigned
+	PayloadStreamingSignedTrailer
+	PayloadStreamingUnsignedTrailer
+)
+
+// TODO: Standardize errors.
+
+type ParsedRequest struct {
+	AccessKeyID   string
+	Timestamp     time.Time
+	Region        Region
+	Service       Service
+	SignedHeaders []string
+	Signature     string
+}
+
+func Parse(req *http.Request, region Region, service Service) (*ParsedRequest, error) {
+	parsed := &ParsedRequest{}
+
+	// Get Authorization header.
+	authHdr := req.Header.Get("Authorization")
+	if authHdr == "" {
+		return nil, fmt.Errorf("no Authorization header present")
+	}
+
+	// Parse Authorization algorithm.
+	algo, remainder, found := strings.Cut(authHdr, " ")
+	if !found {
+		return nil, fmt.Errorf("malformed Authorization header: one and only one ' ' (space) required")
+	}
+	// TODO: When SigV4a support is added, allow AlgorithmV4a to pass this check.
+	if algo != string(AlgorithmV4) {
+		return nil, fmt.Errorf("unsupported Authorization type")
+	}
+
+	// Get contentHdr signature/scheme.
+	contentHdr := req.Header.Get("X-Amz-Content-Sha256")
+	if contentHdr == "" {
+		return nil, fmt.Errorf("missing required header for this request: x-amz-content-sha256")
+	}
+
+	// Parse request timestamp, and save raw date header value (needed for exact signature calculation).
+	var dateHdr string
+	if dateHdr = req.Header.Get("X-Amz-Date"); dateHdr != "" {
+		t, err := time.Parse(amzTimeFormat, dateHdr)
+		if err != nil {
+			// If X-Amz-Date is present (and not empty), but parsing fails, return an error without falling
+			// through to the Date header.
+			return nil, fmt.Errorf("sigV4 authentication requires a valid X-Amz-Date or Date header")
+		}
+		parsed.Timestamp = t
+	} else {
+		// If X-Amz-Date is missing/empty, attempt to use the Date header.
+		dateHdr = req.Header.Get("Date")
+		t, err := http.ParseTime(dateHdr)
+		if err != nil {
+			return nil, fmt.Errorf("sigV4 authentication requires a valid X-Amz-Date or Date header")
+		}
+		parsed.Timestamp = t
+	}
+	// Normalize to UTC for easy downstream comparison.
+	parsed.Timestamp = parsed.Timestamp.UTC()
+
+	// Check current time - timestamp < MaxClockSkew.
+	if skew := time.Since(parsed.Timestamp).Abs(); skew > MaxClockSkew {
+		return nil, fmt.Errorf("the difference between the request time and the current time is too large")
+	}
+
+	// Separate Authorization key=value components, and check we have the right quantity.
+	authHdrParts := strings.Split(remainder, ",")
+	if len(authHdrParts) != 3 {
+		return nil, fmt.Errorf("malformed Authorization header: incorrect number of components provided")
+	}
+
+	// Check we have the correct fields, and get their value strings (without key prefix).
+	cred, credFound := strings.CutPrefix(strings.TrimSpace(authHdrParts[0]), "Credential=")
+	hdrs, hdrsFound := strings.CutPrefix(strings.TrimSpace(authHdrParts[1]), "SignedHeaders=")
+	sig, sigFound := strings.CutPrefix(strings.TrimSpace(authHdrParts[2]), "Signature=")
+	if !credFound || !hdrsFound || !sigFound {
+		return nil, fmt.Errorf("malformed Authorization header: required components are missing or in an incorrect order")
+	}
+
+	// Split the Credential string into it's subcomponents, and check that we have the right amount.
+	credParts := strings.Split(cred, "/")
+	if len(credParts) != 5 {
+		return nil, fmt.Errorf("malformed Authorization header: expected Credential to be in the format \"<YOUR-AKID>/YYYYMMDD/REGION/SERVICE/aws4_request\"")
+	}
+
+	// Confirm the Credential date is in the appropriate format, and that it matches the date
+	// component of the request timestamp.
+	if _, err := time.Parse(amzDateFormat, credParts[1]); err != nil {
+		return nil, fmt.Errorf("malformed Authorization header: the second Credential element must be a date in the format \"YYYYMMDD\"")
+	} else if credParts[1] != parsed.Timestamp.Format(amzDateFormat) {
+		return nil, fmt.Errorf("malformed Authorization header: date does not match X-Amz-Date (or Date, if X-Amz-Date is not set)")
+	}
+
+	// Check that Credential region and service match the endpoint's expected values.
+	parsed.Region = Region(credParts[2])
+	parsed.Service = Service(credParts[3])
+	if parsed.Region != region {
+		return nil, fmt.Errorf("malformed Authorization header: incorrect region \"%s\"; expected \"%s\"", parsed.Region, region)
+	} else if parsed.Service != service {
+		return nil, fmt.Errorf("malformed Authorization header: incorrect service \"%s\"; expected \"%s\"", parsed.Service, service)
+	}
+
+	// Check that Credential has the correct terminal value.
+	if credParts[4] != "aws4_request" {
+		return nil, fmt.Errorf("malformed Authorization header: terminal value; expected \"aws4_request\"", parsed.Service, service)
+	}
+
+	// Assign remaining fields to parsed request envelope.
+	parsed.AccessKeyID = credParts[0]
+	parsed.SignedHeaders = strings.Split(hdrs, ";")
+	parsed.Signature = sig
+
+	return parsed, nil
+}
+
+func (req *ParsedRequest) Verify(secretAccessKey string) error {
+	panic("Verify not implemented")
+}
